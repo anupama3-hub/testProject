02d1127534c02694e5f475f9bbbac728773d26d4
User <User@DESKTOP-KI9J8S8>
2020-12-30 01:47:43 -0800

    comittttttt

--- TestProject/src/com/TestProject.java
+++ TestProject/src/com/TestProject.java
@@ -2,6 +2,6 @@ package com;
 
 public class TestProject {
 public static void main(String[] args) {
-	System.out.println("Test projects    repository");
+	System.out.println("Test projects   repository");
 }
 }
